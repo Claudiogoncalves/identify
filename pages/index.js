3f05bb86e2d3170af1b65f4cb0f4c4d59f2f5fa6
Claudio Goncalves <claudio@MacBook-Pro.Home>
2020-12-26 17:33:30 +0000

    Remove instagram

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faGithub, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF, faGithub, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Layout from '../components/layout';
 import Link from 'next/link';
 import Head from 'next/head';
@@ -32,9 +32,10 @@ function Home() {
 					</header>
           <footer>
             <ul className="icons">
+              <li><a href="https://www.linkedin.com/in/claudiogoncalvesrp/" target="_blank"><span class="label"><FontAwesomeIcon className='test' icon={faLinkedin} size='lg' /></span></a></li>
               <li><a href="https://github.com/Claudiogoncalves" target="_blank"><span class="label"><FontAwesomeIcon className='test' icon={faGithub} size='lg' /></span></a></li>
               <li><a href="https://twitter.com/_claudiorpg" target="_blank" className="icon brands fa-twitter"><FontAwesomeIcon icon={faTwitter} size='lg' /></a></li>
-              <li><a href="https://www.instagram.com/claudiogoncalvesrp/" target="_blank" className="icon brands fa-instagram"><FontAwesomeIcon icon={faInstagram} size='lg' /></a></li>
+              {/* <li><a href="https://www.instagram.com/claudiogoncalvesrp/" target="_blank" className="icon brands fa-instagram"><FontAwesomeIcon icon={faInstagram} size='lg' /></a></li> */}
               <li><a href="https://www.facebook.com/claudiogoncalvesrp" target="_blank" className="icon brands fa-facebook-f"><FontAwesomeIcon
               icon={faFacebookF}
               className='icons'
